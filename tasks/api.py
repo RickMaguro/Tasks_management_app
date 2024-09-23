aa210d05c1f7a4993def0b4dc75b167a60b1cd70
Ahmedq02 <ahmed.abdulq02@gmail.com>
2024-09-23 23:37:05 +0100

    Refactored the api to use custom authentication with async functions, switched to use daphne as the asgi server.

--- tasks/api.py
+++ tasks/api.py
@@ -1,30 +1,47 @@
 from typing import List
 from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI
-from ninja.security import django_auth
+from ninja.security import django_auth, HttpBearer
 from datetime import timedelta
 from django.utils import timezone
 from .models import Task
 from .schemas import TaskSchema, UpdateTaskSchema
+from asgiref.sync import sync_to_async
+from django.contrib.sessions.models import Session
+from django.contrib.auth.models import User
 
-api = NinjaAPI(auth=django_auth)
+api = NinjaAPI()
+
+# funciton that allows authentication in an async setting
+async def async_auth(request):
+    session_key = request.COOKIES.get("sessionid")
+
+    if session_key:
+        session = await sync_to_async(Session.objects.get)(session_key=session_key)
+        user_id = session.get_decoded().get("_auth_user_id")
+        if user_id:
+            user = await sync_to_async(User.objects.get)(id=user_id)
+            return user
+    
+    return None
 
 # POST creates a new task
-@api.post("/tasks/", response={201: TaskSchema})
-def add_task(request, data: TaskSchema):
-    task = Task.objects.create(**data.dict())
+@api.post("/tasks/", response={201: TaskSchema}, auth=async_auth)
+async def add_task(request, data: TaskSchema):
+    task = Task.objects.acreate(**data.dict())
     return task
 
 # GET returns the tasks due in the next 30 days
-@api.get("/tasks/", response=List[TaskSchema])
-def get_tasks(request):
+@api.get("/tasks/", response=List[TaskSchema], auth=async_auth)
+async def get_tasks(request):
     now = timezone.now()
-    tasks = Task.objects.filter(due_by__gte=now, due_by__lte=now + timedelta(days=30)).all()
+    tasks_queryset = Task.objects.filter(due_by__gte=now, due_by__lte=now + timedelta(days=30))
+    tasks = await sync_to_async(list)(tasks_queryset)
     return tasks
 
 # PUT updates a task by id
-@api.put("/tasks/{task_id}/", response=TaskSchema)
-def update_task(request, task_id: int, data: UpdateTaskSchema):
+@api.put("/tasks/{task_id}/", response=TaskSchema, auth=async_auth)
+async def update_task(request, task_id: int, data: UpdateTaskSchema):
     task = get_object_or_404(Task, id=task_id)
     for attr, value in data.dict().items():
         if value is not None:
@@ -33,8 +50,10 @@ def update_task(request, task_id: int, data: UpdateTaskSchema):
     return task
 
 # DELETE deletes a task by id
-@api.delete("/tasks/{task_id}/", response={204: None})
-def delete_task(request, task_id: int):
-    task = get_object_or_404(Task, id=task_id)
-    task.delete()
-    return 204, None
+@api.delete("/tasks/{task_id}/", response={204: None}, auth=async_auth)
+async def delete_task(request, task_id: int):
+    try:
+        task = await Task.objects.aget(id=task_id)
+        await task.adelete()
+    except Task.DoesNotExist:
+        return 404, {"error": "Task not found"}
