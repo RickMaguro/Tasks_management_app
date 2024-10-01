1f39f43a3194b614c6040f2eb71e1e31ef0d6a3c
Ahmedq02 <ahmed.abdulq02@gmail.com>
2024-10-01 20:05:55 +0100

    Switched database to postgresql

--- wellence_project/settings.py
+++ wellence_project/settings.py
@@ -89,9 +89,17 @@ ASGI_APPLICATION = 'wellence_project.asgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_db',  
+        'USER': 'postgres',  
+        'PASSWORD': 'Jjk123!*',  
+        'HOST': 'localhost',  
+        'PORT': '',  
     }
 }
 
