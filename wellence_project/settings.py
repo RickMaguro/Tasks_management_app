c039be507eee0f628ca5c13a904fa184af91c92d
Ahmedq02 <ahmed.abdulq02@gmail.com>
2024-10-10 18:05:02 +0100

    Added azure url to CSRF_TRUSTED_ORIGINS

--- wellence_project/settings.py
+++ wellence_project/settings.py
@@ -67,6 +67,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://ahmed.calmpebble-d0eee67f.uksouth.azurecontainerapps.io',
+]
+
 ROOT_URLCONF = 'wellence_project.urls'
 
 TEMPLATES = [
