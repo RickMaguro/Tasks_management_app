e745a7dd1598ef73dd1c6dfa7d427f3ef22acf45
Ahmedq02 <ahmed.abdulq02@gmail.com>
2024-10-11 23:05:18 +0100

    Fixed entrypoint to check if variables are set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# Ensure all superuser environment variables are set
+if [ -z "$DJANGO_SUPERUSER_USERNAME" ] || [ -z "$DJANGO_SUPERUSER_EMAIL" ] || [ -z "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    echo "Superuser credentials are not fully set."
+    exit 1
+fi
+
 python manage.py migrate
 
 # Check if a superuser exists, and if not, create one
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists(): \
-    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD'); \
-    print('Superuser created.'); \
-else: \
-    print('Superuser already exists.');"
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username='$DJANGO_SUPERUSER_USERNAME';
+email='$DJANGO_SUPERUSER_EMAIL';
+password='$DJANGO_SUPERUSER_PASSWORD';
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password);
+    print('Superuser created.');
+else:
+    print('Superuser already exists.');
+"
 
 # Start the server
 exec "$@"
